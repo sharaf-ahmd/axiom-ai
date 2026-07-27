bc7bb60fd7b553aff58165a82fb6dbfdc9fe6528
Sharaf <sharaf.ams.11@gmail.com>
2026-07-27 19:30:43 +0530

    Initial bug fixes

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -17,9 +17,7 @@ class Settings(BaseSettings):
 
     JWT_ALGORITHM: str = "HS256"
 
-
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 settings = Settings()
